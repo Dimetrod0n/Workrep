a19c8762470a47178c48ec85603fa1f84a68c867
Dimetrod0n <44606970+Dimetrod0n@users.noreply.github.com>
2018-11-06 23:09:22 +0300

    Update run.tf

--- run.tf
+++ run.tf
@@ -1,4 +1,3 @@
-
 variable "subscription_id" {}
 variable "tenant_id" {}
 variable "client_id" {}
@@ -15,8 +14,87 @@ provider "azurerm" {
 resource "azurerm_resource_group" "myterraformgroup" {
     name     = "DmitrysGroup"
     location = "eastus"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+
  }
 
+
+#Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "DmitrysVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+
+#Create subnet 
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "DmitrysSubnet"
+    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
+    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+    address_prefix       = "10.0.2.0/24"
+}
+
+#Create public IP address
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "DmitrysPublicIP"
+    location                     = "eastus"
+    resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
+    public_ip_address_allocation = "dynamic"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+#Create Network Security Group
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "DmitrysNetworkSecurityGroup"
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_network_interface" "myterraformnic" {
+    name                = "DmitrysNIC"
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+    network_security_group_id = "${azurerm_network_security_group.myterraformnsg.id}"
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+        private_ip_address_allocation = "dynamic"
+        public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
+    }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
 
 #Create VM
 resource "azurerm_resource_group" "VM" {
