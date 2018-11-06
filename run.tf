0c28bdef5595c1ca2a85a477ed2e9eccec083c77
Dimetrod0n <44606970+Dimetrod0n@users.noreply.github.com>
2018-11-06 19:50:11 +0300

    Update run.tf

--- run.tf
+++ run.tf
@@ -26,21 +26,3 @@ resource "azurerm_resource_group" "VM" {
     command = "./azvmcreate.txt"
    }
   }
-
-#Open Jenkins port
-resource "azurerm_resource_group" "Jenkinsport" {
-  name     = "DmitrysGroup"
-  location = "eastus"
-  provisioner "local-exec" {
-    command = "./open_Jenkins_port.txt"
-   }
-  }
-
-#Open Node.js port
-resource "azurerm_resource_group" "Nodejsport" {
-  name     = "DmitrysGroup"
-  location = "eastus"
-  provisioner "local-exec" {
-    command = "./open_nodejs_port.txt"
-   }
-  }
