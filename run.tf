fe46d8fb06e873c0c574c070fd340c6af169adfe
Dimetrod0n <44606970+Dimetrod0n@users.noreply.github.com>
2018-11-06 17:06:35 +0300

    Update run.tf

--- run.tf
+++ run.tf
@@ -12,26 +12,34 @@ provider "azurerm" {
 }
 
 # Give execute privilege
+resource "insatance" "my" {
   provisioner "local-exec" {
     command = "chmod +x /home/dmitry/WorkDir/azvmcreate.txt"
     command = "chmod +x /home/dmitry/WorkDir/cloud-init-jenkins.txt"
     command = "chmod +x /home/dmitry/WorkDir/run.tf"
     command = "chmod +x /home/dmitry/WorkDir/open_ports.txt"
   }
+}
 
 
 #Create resource group
+resource "insatance" "my" {
 resource "azurerm_resource_group" "myterraformgroup" {
     name     = "DmitrysGroup"
     location = "eastus"
+ }
 }
 
 #Create VM
+resource "insatance" "my" {
   provisioner "local-exec" {
     command = "./azvmcreate.txt"
+   }
   }
 
 #Open ports
+resource "insatance" "my" {
   provisioner "local-exec" {
     command = "./open_ports.txt"
+   }
   }
