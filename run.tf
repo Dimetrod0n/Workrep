86d8bceaa844aeeb5a4f3b081346d12800e24d45
Dimetrod0n <44606970+Dimetrod0n@users.noreply.github.com>
2018-11-06 15:35:00 +0300

    Update run.tf

--- run.tf
+++ run.tf
@@ -1,4 +1,9 @@
-provider "azurerm" {}
+provider "azurerm" {
+    subscription_id = "${var.subscription_id}"
+    tenant_id = "${var.tenant_id}"
+    client_id = "${var.client_id}"
+    client_secret = "${var.client_secret}"
+}
 
 # Copies files to WorkDir
   provisioner "file" {
