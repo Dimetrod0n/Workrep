108195d4a8199e78826665ba953962a1f34853c1
Dimetrod0n <44606970+Dimetrod0n@users.noreply.github.com>
2018-11-06 16:02:41 +0300

    Update run.tf

--- run.tf
+++ run.tf
@@ -1,3 +1,9 @@
+
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+
 provider "azurerm" {
     subscription_id = "${var.subscription_id}"
     tenant_id = "${var.tenant_id}"
