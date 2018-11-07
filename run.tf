82ba6c9864b4779505cbf04aca7ac6c58e603e62
Dimetrod0n <44606970+Dimetrod0n@users.noreply.github.com>
2018-11-07 12:22:31 +0300

    Update run.tf

--- run.tf
+++ run.tf
@@ -68,7 +68,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         access                     = "Allow"
         protocol                   = "Tcp"
         source_port_range          = "*"
-        destination_port_ranges     = ["8081","1338"]
+        destination_port_range     = "22"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
@@ -77,7 +77,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         environment = "Terraform Demo"
     }
 }
-
+#Create network interface
 resource "azurerm_network_interface" "myterraformnic" {
     name                = "DmitrysNIC"
     location            = "eastus"
@@ -96,11 +96,71 @@ resource "azurerm_network_interface" "myterraformnic" {
     }
 }
 
+#Generate randomId for next step
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = "${azurerm_resource_group.myterraformgroup.name}"
+    }
+
+    byte_length = 8
+}
+
+#Create storage account
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                = "Dmitrysdiag${random_id.randomId.hex}"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+    location            = "eastus"
+    account_replication_type = "LRS"
+    account_tier = "Standard"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
 #Create VM
-resource "azurerm_resource_group" "VM" {
-  name     = "DmitrysGroup"
-  location = "eastus"
-  provisioner "local-exec" {
-    command = "./azvmcreate.txt"
-   }
-  }
+resource "azurerm_virtual_machine" "myterraformvm" {
+    name                  = "DmitrysVM"
+    location              = "eastus"
+    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+    network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_os_disk {
+        name              = "DmitrysOsDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04.0-LTS"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "Dmitrysvm"
+        admin_username = "dmitry"
+        admin_password = "Password1234!"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+        ssh_keys {
+            path     = "/home/azureuser/.ssh/authorized_keys"
+            key_data = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
+        }
+    }
+
+    boot_diagnostics {
+        enabled     = "true"
+        storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
+    }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
