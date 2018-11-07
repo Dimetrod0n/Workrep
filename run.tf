e8aee570e1ebfa15eb7b3e2fb49f93ff11b09662
Dimetrod0n <44606970+Dimetrod0n@users.noreply.github.com>
2018-11-07 12:57:40 +0300

    Update run.tf

--- run.tf
+++ run.tf
@@ -95,16 +95,6 @@ resource "azurerm_network_interface" "myterraformnic" {
     }
 }
 
-#Generate randomId for next step
-#resource "random_id" "randomId" {
-#    keepers = {
-#        # Generate a new ID only when a new resource group is defined
-#       resource_group = "${azurerm_resource_group.myterraformgroup.name}"
-#    }
-#
-#    byte_length = 8
-#}
-
 #Create storage account
 resource "azurerm_storage_account" "mystorageaccount" {
     name                = "dimetrod0n"
