25302c6c4095d7d366d31704fd69d6196fe53c93
Dimetrod0n <44606970+Dimetrod0n@users.noreply.github.com>
2018-11-08 12:14:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,5 +9,4 @@ var server = http.createServer(function(request, response) {
 var port = process.env.PORT || 1338;
 server.listen(port);
 
-
 console.log("Server running at http://localhost:%d", port);
